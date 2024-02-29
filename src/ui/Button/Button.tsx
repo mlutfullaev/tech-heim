cce82a6501a34e28b2bff8e3d2c4f58609dc3131
magalutfullaev <mhoja9494@gmail.com>
2024-02-29 12:38:05 +0500

    add: Props to button component

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ReactPropTypes} from "react";
 import cls from './Button.module.sass'
 import {ButtonColor, ButtonType} from "@/assets/js/types.ts";
 
@@ -7,14 +7,16 @@ type ButtonT = {
   type?: ButtonType,
   disabled?: boolean;
   classNames?: string;
+  props?: ReactPropTypes
   children: React.ReactNode
 }
 
-const Button = ({color, type, children, disabled, classNames}: ButtonT) => {
+const Button = ({color, type, children, disabled, classNames, props}: ButtonT) => {
   return (
     <button
       disabled={disabled}
       className={`${cls.button} ${color ? cls[color] : cls.blue} ${type ? cls[type] : cls.solid} ${classNames}`}
+      {...props}
     >
       {children}
     </button>
